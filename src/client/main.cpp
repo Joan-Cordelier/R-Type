2d693cbd16aee8b2ff8058f195d86ee8e3728e8d
DarquesTheo <theo.darques@epitech.eu>
2025-11-26 21:39:23 +0100

    ADD: input queue system

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -11,18 +11,24 @@ int main(void)
 {
     Renderer renderer;
     std::string id = renderer.loadTexture("a.png", "test");
-    std::string idFont = renderer.loadFont("font.ttf", 16, "mainFont");
 
     bool running = true;
 
     while (running) {
-        PollEvent status = renderer.window.pollEvent();
-        if (status.type == PollStatus::QUIT)
-            break;
+        SDL_Event evt = renderer.window.pollEvent();
+        while (evt.type != 0) {
+            switch(evt.type) {
+                case SDL_QUIT:
+                    running = false;
+                    break;
+                default:
+                    break;
+            }
+            evt = renderer.window.pollEvent();
+        }
 
         renderer.clear();
         renderer.drawTexture("test", {100, 100, 100, 100}, {.rotation = 45.0f, .alpha = 100, .tint = Color(255, 255, 255)});
-        renderer.drawFont("mainFont", "Anto gay !", 50, 50, Color(255, 255, 255));
         renderer.render();
     }
 
