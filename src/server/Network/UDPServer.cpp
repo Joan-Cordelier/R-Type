fa667c92b85be1e0451eef00a1cb2b0eb17995de
tristan-ugh <tristan.ughetto@epitech.eu>
2025-11-26 15:45:53 +0100

    ADD(server): logger

--- src/server/Network/UDPServer.cpp
+++ src/server/Network/UDPServer.cpp
@@ -6,6 +6,8 @@
 */
 
 #include "UDPServer.hpp"
+#include "../Logs/Logger.hpp"
+#include <arpa/inet.h>
 
 UDPServer::UDPServer(ThreadedQueue& queue) : AServer(queue)
 {
@@ -23,8 +25,10 @@ int UDPServer::run()
     MessageFactory& factory = MessageFactory::getInstance();
 
     while (_running) {
-        if (poll(_fds.data(), _fds.size(), 100) < 0)
+        if (poll(_fds.data(), _fds.size(), 100) < 0) {
+            LOG_ERROR("UDP poll failed");
             return 84;
+        }
         for (size_t i = 0; i < _fds.size(); ++i) {
             if (_fds[i].revents & POLLIN) {
                 char buffer[4096];
@@ -33,19 +37,28 @@ int UDPServer::run()
                 int n = recvfrom(_serverFd, buffer, sizeof(buffer), 0, (struct sockaddr*)&clientAddr, &len);
                 if (n > 0) {
                     bool isNewClient = false;
+                    std::string clientIp = inet_ntoa(clientAddr.sin_addr);
+                    int clientPort = ntohs(clientAddr.sin_port);
                     {
                         std::lock_guard<std::mutex> lock(_clientsMutex);
                         isNewClient = std::find_if(_clients.begin(), _clients.end(), [&clientAddr](const sockaddr_in& addr) {
                                 return addr.sin_addr.s_addr == clientAddr.sin_addr.s_addr &&addr.sin_port == clientAddr.sin_port;
                             }) == _clients.end();
                         
-                        if (isNewClient)
+                        if (isNewClient) {
                             _clients.push_back(clientAddr);
+                            LOG_INFO("UDP client connected (" + clientIp + ":" + std::to_string(clientPort) + ")");
+                        }
                     }
+                    LOG_DEBUG("UDP received " + std::to_string(n) + " bytes from " + clientIp + ":" + std::to_string(clientPort));
                     std::vector<uint8_t> rawData(buffer, buffer + n);
                     DecodedMessage msg = factory.decode(rawData);
-                    if (msg.opCode != PARSING_ERROR)
+                    if (msg.opCode != PARSING_ERROR) {
+                        LOG_INFO("UDP message received: OpCode=" + std::to_string(msg.opCode) + " Len=" + std::to_string(msg.len));
                         _queue.push(msg.priority, msg.data);
+                    } else {
+                        LOG_WARN("UDP parsing error from " + clientIp + ":" + std::to_string(clientPort));
+                    }
                 }
             }
         }
@@ -55,14 +68,22 @@ int UDPServer::run()
 
 int UDPServer::send(const MessageData& data, const sockaddr_in& clientAddr)
 {
+    std::string clientIp = inet_ntoa(clientAddr.sin_addr);
+    int clientPort = ntohs(clientAddr.sin_port);
     ssize_t sent = sendto(_serverFd, data.data(), data.size(), 0, (struct sockaddr*)&clientAddr, sizeof(clientAddr));
     
-    return (sent < 0) ? -1 : 0;
+    if (sent < 0) {
+        LOG_ERROR("UDP send failed to " + clientIp + ":" + std::to_string(clientPort));
+        return -1;
+    }
+    LOG_DEBUG("UDP sent " + std::to_string(data.size()) + " bytes to " + clientIp + ":" + std::to_string(clientPort));
+    return 0;
 }
 
 int UDPServer::send(const MessageData& data)
 {
     std::lock_guard<std::mutex> lock(_clientsMutex);
+    LOG_DEBUG("UDP broadcasting " + std::to_string(data.size()) + " bytes to " + std::to_string(_clients.size()) + " clients");
     for (const auto& clientAddr : _clients) {
         sendto(_serverFd, data.data(), data.size(), 0,
                (struct sockaddr*)&clientAddr, sizeof(clientAddr));
