d0255d963f834e4e3b942edd4efbea91d0ef85b4
totosaure <antonin.veau@epitech.eu>
2025-12-08 16:25:00 +0100

    FIX : client running removed

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -128,8 +128,6 @@ int main()
         MessageFactory& factory = MessageFactory::getInstance();
         PreparedMessage msg = factory.createMessage(OpCode::CONNECT, {});
         network.sendTcp(msg);
-        network.getUdpClient().connect();
-        network.getUdpClient().run();
     });
 
     while (running) {
