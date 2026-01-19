c2cef61b111ace74384abfa1d53299628bd9a736
Joan-Cordelier <Joan-Cordelier@users.noreply.github.com>
2026-01-19 08:08:47 +0000

    deploy: 584d2497543dcf8bfd0f15b88093dc3b36663fab

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8ab67c50.js',
+                path_to_root + 'searchindex-80403e95.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
